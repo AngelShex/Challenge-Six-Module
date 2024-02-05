813bffec1d0395acd41e32332a9c59fd3fe5d9c4
Angel Shelton <angelshelton@Angels-MacBook-Air.local>
2024-02-05 06:45:27 -0800

    calling weather api key forcast and area using open weather website

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ const searchButton = document.querySelector(".search-btn");
 
 const API_KEY = "67c516a9d98b31f230ca4b47e49f1ea4";
 
+const getWeatherDetails = (cityName, lat, lon) => {
+    const WEATH_API_URL = `api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={API key}`;
+}
+
 const getCityCoordinates = () => {
     const cityName = cityInput.value.trim(); 
     if(!cityName) return;
 const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
     
 fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
-    console.log(data)
+    if(!data.length) return alert(`No coordinates found for ${cityName}`);
+    const { name, lat, lon } = data[0];
+    getWeatherDetails(name, lat, lon);
 }).catch(() => {
 alert("An error occurred while featching the coordinates!");
 });
