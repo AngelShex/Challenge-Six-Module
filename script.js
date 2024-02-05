385f363223e6b98d569a16da522abc39e51913d8
Angel Shelton <angelshelton@Angels-MacBook-Air.local>
2024-02-05 06:34:50 -0800

    working on fetching data from open weather website using the geoo coding api

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ const getCityCoordinates = () => {
     if(!cityName) return;
 const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
     
+fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
+    console.log(data)
+}).catch(() => {
+alert("An error occurred while featching the coordinates!");
+});
 }
 
 searchButton.addEventListener("click", getCityCoordinates);
