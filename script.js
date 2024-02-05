80303be96e10cecd6b45afa82c5100e586ed4984
Angel Shelton <angelshelton@Angels-MacBook-Air.local>
2024-02-05 10:20:40 -0800

    fixing java script file with city search input

--- script.js
+++ script.js
@@ -30,9 +30,9 @@ return `   <div class="details">
     }
 
 const getWeatherDetails = (cityName, lat, lon) => {
-    const WEATH_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt={cnt}&appid=${API_KEY}`;
+    const GEOCODING_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt={cnt}&appid=${API_KEY}`;
 
-fetch(WEATH_API_URL).then(res => res.json()).then(data => {
+fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
 
     const uniqueForecastDays = [];
   const fiveDaysForecast = data.list.filter(forecast => {
@@ -65,6 +65,7 @@ const getCityCoordinates = () => {
     if(!cityName) return;
 const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
     
+// When entered can retreive city coordinates (lat, long, and name) from API
 fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
     if(!data.length) return alert(`No coordinates found for ${cityName}`);
     const { name, lat, lon } = data[0];
@@ -96,6 +97,6 @@ const getUserCoordinates = () => {
 }
 
 
-locationButtonButton.addEventListener("click", getUserCoordinates);
+locationButton.addEventListener("click", getUserCoordinates);
 searchButton.addEventListener("click", getCityCoordinates);
-cityInput.addEventListener("keyup", getCityCoordinates);
+cityInput.addEventListener("keyup", e => e.key === "Enter" && getCityCoordinates());
