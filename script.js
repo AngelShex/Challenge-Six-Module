f3ed031e7d9f788cd8ed0cbf76e58a509e50ddaf
Angel Shelton <angelshelton@Angels-MacBook-Air.local>
2024-02-05 06:26:05 -0800

    linking open weather api key and function within my java script file

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
 
+const API_KEY = "67c516a9d98b31f230ca4b47e49f1ea4";
+
 const getCityCoordinates = () => {
     const cityName = cityInput.value.trim(); 
     if(!cityName) return;
-
-    console.log(cityName)
+const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+    
 }
 
 searchButton.addEventListener("click", getCityCoordinates);
