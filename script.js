488f743b4d2debd42396886e0dbb48578df517c5
Angel Shelton <angelshelton@Angels-MacBook-Air.local>
2024-02-07 18:41:33 -0800

    added edits to java

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
 const locationButton = document.querySelector(".location-btn");
