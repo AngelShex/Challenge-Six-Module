7d3cb44a4660d249804548a883c46068962cd8a1
Angel Shelton <angelshelton@Angels-MacBook-Air.local>
2024-02-10 22:06:15 -0800

    added tags into java script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// query tags 
 const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
 const locationButton = document.querySelector(".location-btn");
